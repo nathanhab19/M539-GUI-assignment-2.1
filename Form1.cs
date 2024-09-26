8347449c53d636f053b73cadb6ab9489fecdc147
nathanhab19 <nathanhab19@gmail.com>
2024-09-25 19:40:25 -0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M539_GUI_assignment_2._1
+    //Nathan Habimana 
+    //M539 2.1 GUI Assignment
+    //est time 30 minutes
+    //actual time 10 minutes, didnt do any code, just GUI interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true; ;
+            System.Diagnostics.Process.Start("https://www.calculator.net/bmr-calculator.html");
+
+        }
+    }
+}
